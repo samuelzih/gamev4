75cb14fcc820dd995733b0d99a8a06d39a57a00e
samuelzih <samuelz@instaacoders.com>
2023-04-15 11:26:09 +0530

    your commit message

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(sessionMiddleware);
 
 var server = require("http")
 	.createServer(app)
-	.listen(process.env.PORT || 3000, () => {
+	.listen(process.env.PORT || 4000, () => {
 		var port = server.address()?.port;
 		console.log(`Server is running on portzznew ` + port);
 	});
