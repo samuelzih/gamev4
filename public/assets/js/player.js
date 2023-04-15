a772360beedc7cf942ee968cd9f803cc0ebfec39
samuelzih <samuelz@instaacoders.com>
2023-04-15 12:02:00 +0530

    your commit message

--- public/assets/js/player.js
+++ public/assets/js/player.js
@@ -156,7 +156,7 @@ jQuery(
 		var IO = {
 			init: function () {
 				// IO.socket = io('http://ec2-35-170-246-227.compute-1.amazonaws.com:3000/');
-				IO.socket = io("http://44.201.194.58:4000/");
+				IO.socket = io("http://54.146.213.208:3000/");
 				IO.bindEvents();
 			},
 			bindEvents: function () {
